fdc9f9242788306979f191d2809b61725de1d40d
Athensoft <inf.athensoft@hotmail.com>
2019-11-19 01:07:51 -0500

    added AthensMain

---
+++ src/com/athensoft/cdci/github/AthensMain.java
@@ -0,0 +1,10 @@
+package com.athensoft.cdci.github;
+
+public class AthensMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("yes");
+	}
+
+}
