be41548344401e439058df2e178a036436c188c8
Athensoft <inf.athensoft@hotmail.com>
2019-11-19 00:50:42 -0500

    added client app

---
+++ src/com/athensoft/cdci/github/ClientApp.java
@@ -0,0 +1,11 @@
+package com.athensoft.cdci.github;
+
+public class ClientApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("cdci hello");
+
+	}
+
+}
